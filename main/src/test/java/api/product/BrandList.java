c6d4ed95ee331646fa2c6e26ee7427a8cce0cca4
rogelio-brill <rogelio.aguilar@brillio.com>
2022-03-02 15:43:10 -0800

    Completed brands list api tests

--- main/src/test/java/api/product/BrandList.java
+++ main/src/test/java/api/product/BrandList.java
@@ -14,53 +14,44 @@ import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 
-public class ProductList {
+public class BrandList {
 
     @BeforeSuite
     public static void setup() {
         // Base URL and URI for all REST API requests
-        RestAssured.baseURI = "https://automationexercise.com/api/productsList";
+        RestAssured.baseURI = "https://automationexercise.com/api/brandsList";
     }
 
     @Test()
-    public void getProductList() {
+    public void getBrandsList() {
         // Stores get response in res variable
         Response res = given().relaxedHTTPSValidation().contentType(ContentType.JSON).
-        when().get().
-        then().statusCode(200).extract().response();
+                when().get().
+                then().statusCode(200).extract().response();
 
         //convert JSON to string
         JsonPath j = new JsonPath(res.asString());
         //length of JSON array
-        int s = j.getInt("products.size()");
+        int s = j.getInt("brands.size()");
 
         // Checks that there are the expectd amount of products
         Assert.assertEquals(s, 34);
     }
 
     @Test()
-    public void postProduct() {
+    public void putBrand() {
         // JSON body that will be passed to POST API
         Map<String, Object> data = new HashMap<>();
-        data.put("id", "44");
-        data.put("name", "Red Top");
-        data.put("price", "Rs. 500");
-        data.put("brand", "Polo");
-        data.put("category", Arrays.asList(new HashMap<String, Object>() {{
-                                             put("usertype", Arrays.asList(new HashMap<String, Object>() {{
-                                                 put("usertype", "Men");
-                                             }}));
-                                             put("category", "Tshirts");
-                                         }}
-        ));
+        data.put("id", "1");
+        data.put("brand", "Ralph Lauren");
 
         // Gathers response of POST request
         Response res = given().relaxedHTTPSValidation().contentType(ContentType.JSON).body(data)
-                .when().post()
+                .when().put()
                 .then()
-                    .statusCode(200)
-                    .log().body()
-                    .extract().response();
+                .statusCode(200)
+                .log().body()
+                .extract().response();
 
         // Stores POST response into string and checks if correct response is returned
         String jsonString = res.asPrettyString();
