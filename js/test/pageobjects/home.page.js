8c28ac8ec94eb542da8dc3740c865fe4529a61b6
capstone-git <capstone.git@gmail.com>
2022-03-07 19:37:46 -0800

    merge conflict solve

--- js/test/pageobjects/home.page.js
+++ js/test/pageobjects/home.page.js
@@ -1,6 +1,20 @@
 const Page = require('./page');
 
 class HomePage extends Page {
+    /**
+     * define selectors using getter methods
+     */
+
+    get testCasesbtn() {
+        return $('/html/body/header/div/div/div/div[2]/div/ul/li[5]/a');
+    }
+
+    /**
+     * a method to encapsule automation code to interact with the page
+     */
+    async testCases () {
+        await this.testCasesbtn.click();
+    }
 
     open() {
         return super.open('');
